3da8c9f21a7dbdac055fc4bd7ff6c19875e7d4d8
pagdayawonjohnivan@gmail.com <Newpass1209!@test>
2021-01-24 19:13:51 +0800

    fix navbar alignment

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,45 +8,23 @@ const Header = (props) => {
     return (
         <nav className="header navbar-fixed-top navbar-principal">
             <div className="container">
-                <div className="navbar-header logo">
-                    Mybook
-            </div>
-                <div id="navbar" className="collapse navbar-collapse">
-                    <div className="col-md-5 col-sm-3">
-                        <form className="navbar-form">
-                            <div className="form-group" style={{ display: "inline" }}>
-                                <div className="input-group" style={{ display: "table" }}>
-                                    <input className="form-control" name="search" placeholder="Search..." autocomplete="off" type="text" />
-                                    <span className="input-group-addon" style={{ width: "1%" }}>
-                                        <span className="glyphicon glyphicon-search"></span>
-                                    </span>
-                                </div>
-                            </div>
+                <div className="navbar-container row" style={{ alignItems: "center" }}>
+                    <div className="navbar-header logo col-md-1">
+                        Mybook
+                    </div>
+                    <div className="col-md-4 col-sm-3">
+                        <form class="navbar-form" style={{ maxWidth: 300 }}>
+                            <input type="text" className="form-control" placeholder="Search.." name="search2" autocomplete="off" style={{ outline: "none" }} />
+                            <button type="submit"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="profile.html">
-                                Katya
-                          <img src="img/Friends/woman-4.jpg" className="img-nav" />
-                            </a>
-                        </li>
-                        <li className="active"><a href="home.html"><i className="fa fa-bars"></i>&nbsp;Home</a></li>
-                        <li><a href="messages.html"><i className="fa fa-comments"></i></a></li>
-                        <li className="dropdown">
-                            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">	Pages
-                          <span className="caret"></span>
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a href="photos.html">Photos</a></li>
-                                <li><a href="view_photo.html">View photo</a></li>
-                                <li><a href="friends.html">Friends</a></li>
-                                <li><a href="user_detail.html">User detail</a></li>
-                                <li><a href="list_users.html">List users</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#" className="nav-controller"><i className="fa fa-user"></i></a></li>
-                    </ul>
+                    <div className="col-md-7" >
+                        <ul className="nav navbar-right">
+                            <li className="active"><a href="home.html"><i className="fa fa-bars"></i>&nbsp;Home</a></li>
+                            <li><a href="messages.html"><i className="fa fa-comments"></i></a></li>
+                            <li><a href="#" className="nav-controller"><i className="fa fa-user"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav >
